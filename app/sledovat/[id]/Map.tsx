0b41663109647e6d2091ac6b840600d9839ae0ff
MacBook <macbook@MacBook-Pro-uzivatela-MacBook.local>
2025-12-02 14:07:47 +0100

    Use iframe map instead of leaflet

--- app/sledovat/[id]/Map.tsx
+++ app/sledovat/[id]/Map.tsx
@@ -1,26 +1,13 @@
 'use client'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import L from 'leaflet'
-import 'leaflet/dist/leaflet.css'
-
-const icon = L.icon({
-  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
-  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
-  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-})
 
 export default function Map({ lat, lng }: { lat: number, lng: number }) {
+  const mapUrl = `https://www.openstreetmap.org/export/embed.html?bbox=${lng-0.01},${lat-0.01},${lng+0.01},${lat+0.01}&layer=mapnik&marker=${lat},${lng}`
+  
   return (
-    <MapContainer center={[lat, lng]} zoom={15} style={{ height: '100%', width: '100%' }}>
-      <TileLayer
-        attribution='&copy; OpenStreetMap'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[lat, lng]} icon={icon}>
-        <Popup>Kurier je tu</Popup>
-      </Marker>
-    </MapContainer>
+    <iframe 
+      src={mapUrl}
+      style={{ width: '100%', height: '100%', border: 0 }}
+      loading="lazy"
+    />
   )
 }
