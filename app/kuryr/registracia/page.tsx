d8fcf3d7e735a7e0e1ebfb45707510e8d8bae2fc
MacBook <macbook@MacBook-Pro-uzivatela-MacBook.local>
2025-12-07 00:26:32 +0100

    Add Turnstile to courier registration properly

--- app/kuryr/registracia/page.tsx
+++ app/kuryr/registracia/page.tsx
@@ -438,7 +438,7 @@ export default function CourierRegistration() {
                     />
                   </div>
                 </>
-            )}
+              )}
             </div>
           )}
 
